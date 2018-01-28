afec9700f750dc2e42021d77a12a9e580f8bd6b1
pickardb <blakepickard@hotmail.com>
2018-01-27 16:39:32 -0800

    Updated 1.4 code, worked on another team's touchscreen, just need a new touchscreen

--- exercise1_4/NIOS_II_SYSTEM_391_For_Students/software/exercise1_4/hello_world.c
+++ exercise1_4/NIOS_II_SYSTEM_391_For_Students/software/exercise1_4/hello_world.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define Touch_Status 	(*(volatile unsigned char *)(0x84000230))
-#define Touch_Control 	(*(volatile unsigned char *)(0x84000230))
-#define Touch_Tx 		(*(volatile unsigned char *)(0x84000232))
-#define Touch_Rx 		(*(volatile unsigned char *)(0x84000232))
-#define Touch_Baud 		(*(volatile unsigned char *)(0x84000234))
+#define Touchscreen_Status 	(*(volatile unsigned char *)(0x84000230))
+#define Touchscreen_Control 	(*(volatile unsigned char *)(0x84000230))
+#define Touchscreen_TxData 		(*(volatile unsigned char *)(0x84000232))
+#define Touchscreen_RxData 		(*(volatile unsigned char *)(0x84000232))
+#define Touchscreen_Baud 		(*(volatile unsigned char *)(0x84000234))
 
 //Touch Enable 				0x55 0x01 0x12
 //Touch Enable Response 	0x55 0x02 STATUS 0x12
@@ -18,81 +18,74 @@ typedef struct { int x, y; } Point ;
 
 
 
+int putCharTouch(char c){
+	// Wait for TX bit in status register to turn 1 (means transmit is empty)
+	while((Touchscreen_Status & 0x02) != 0x02);
+	// Send the data to TX (reduce to 8 bits)
+	Touchscreen_TxData = c;
+	return c & 0xFF;
+}
+
+int getCharTouch(void)
+{
+	// poll Rx bit in 6850 status register. Wait for it to become '1'
+	// read received character from 6850 RxData register.
+	while ((Touchscreen_Status & 0x01) != 0x01);
+	return Touchscreen_RxData;
+}
+
 /*****************************************************************************
- ** Initialise touch screen controller
- *****************************************************************************/
+** Initialise touch screen controller
+*****************************************************************************/
 void Init_Touch(void)
 {
-	// Program 6850 and baud rate generator to communicate with touchscreen
-	// send touchscreen controller an "enable touch" command
-	printf("Initializing Touch Screen\n");
-	Touch_Control = 0x03;
-	Touch_Control = 0x15;
-	Touch_Baud = 0b00000101; //Program for 9600 baud
-	int i=0;
-		while(i<1000){
-			i++;
-		}
+	 // Program 6850 and baud rate generator to communicate with touchscreen
+	 // send touchscreen controller an "enable touch" command
+
+	// Divide by 16 clock, RTS Low, 8 bits of data, no parity,
+	// 1 stop bit, transmitter interrupt disabled
+	Touchscreen_Control = 0x15;
+	// 9600 BAUD
+	Touchscreen_Baud = 0x05;
+
+	// slight delay to process
+	usleep(10000);
+
+	// Send TOUCH_ENABLE command
 	putCharTouch(0x55);
 	putCharTouch(0x01);
 	putCharTouch(0x12);
-	i=0;
-		while(i<1000){
-			i++;
-		}
 
+	usleep(10000);
 }
+
 /*****************************************************************************
- ** test if screen touched
- *****************************************************************************/
+** test if screen touched
+*****************************************************************************/
 int ScreenTouched( void )
 {
-	// return TRUE if any data received from 6850 connected to touchscreen
-	// or FALSE otherwise
-	return (Touch_Rx == 0x80);
+	 // return TRUE if any data received from 6850 connected to touchscreen
+	 // or FALSE otherwise
 
+	return (Touchscreen_RxData == 0x80);
 }
+
 /*****************************************************************************
- ** wait for screen to be touched
- *****************************************************************************/
+** wait for screen to be touched
+*****************************************************************************/
 void WaitForTouch()
 {
-	while(!ScreenTouched()){}
+	while(!ScreenTouched())
+		;
 }
-/* a data type to hold a point/coord */
-/*****************************************************************************
- * This function waits for a touch screen press event and returns X,Y coord
- *****************************************************************************/
 
-
-int ScreenReleased( void )
-{
-	// return TRUE if any data received from 6850 connected to touchscreen
-	// or FALSE otherwise
-	return (Touch_Rx == 0x81);
-
-}
-/*****************************************************************************
- ** wait for screen to be released
- *****************************************************************************/
-void WaitForRelease()
-{
-	while(!ScreenReleased()){}
-}
-/* a data type to hold a point/coord */
-/*****************************************************************************
- * This function waits for a touch screen press event and returns X,Y coord
- *****************************************************************************/
-
-Point GetPress(void)
-{
+Point GetPen(void){
 	Point p1;
 	int packets[4];
 	// wait for a pen down command then return the X,Y coord of the point
 	// calibrated correctly so that it maps to a pixel on screen
 
 	// Wait for first packet of touch
-	printf("Waiting for Touch \n");
 	WaitForTouch();
 
 	int i;
@@ -108,71 +101,47 @@ Point GetPress(void)
 	p1.x = p1.x * 799 / 4095;
 	p1.y = p1.y  * 479 / 4095;
 
-	printf("Press x = %d ", p1.x);
-	printf("Press y = %d\n", p1.y);
+	printf("x = %d ", p1.x);
+	printf("y = %d\n", p1.y);
 
 	return p1;
+}
+
+/*****************************************************************************
+* This function waits for a touch screen press event and returns X,Y coord
+*****************************************************************************/
+Point GetPress(void)
+{
+	 Point p1;
+	 // wait for a pen down command then return the X,Y coord of the point
+	 // calibrated correctly so that it maps to a pixel on screen
+
+	 return GetPen();
 }
 /*****************************************************************************
- * This function waits for a touch screen release event and returns X,Y coord
- *****************************************************************************/
+* This function waits for a touch screen release event and returns X,Y coord
+*****************************************************************************/
 Point GetRelease(void)
 {
-	Point p1;
-	int packets[4];
-	// wait for a pen down command then return the X,Y coord of the point
-	// calibrated correctly so that it maps to a pixel on screen
-
-	// Wait for first packet of touch
-	printf("Waiting for release\n");
-	WaitForRelease();
-
-	int i;
-	for(i = 0; i < 4; i++){
-		packets[i] = getCharTouch();
-	}
-
-	// Get x11 : x7 from 2nd packet, and concatenate to x6 : x0 from 1st packet
-	p1.x = (packets[1] << 7) | packets[0];
-	p1.y = (packets[3] << 7) | packets[2];
-
-	// Map from controller resolution to screen pixel
-	p1.x = p1.x * 799 / 4095;
-	p1.y = p1.y  * 479 / 4095;
-
-	printf("Release x = %d ", p1.x);
-	printf("Release y = %d\n", p1.y);
-
-	return p1;
-}
-int putCharTouch(int c) {
-	// Poll tx bit in 6850 status register. Wait for it to become '1'
-	// Write 'c' to the 6850 TxData register to output the character
-	int read_status_bit = 0;
-	while(read_status_bit == 0) {
-		read_status_bit = Touch_Status & 0b10;
-	}
-	Touch_Tx = c;
-	return c;
+	 Point p1;
+	 // wait for a pen up command then return the X,Y coord of the point
+	 // calibrated correctly so that it maps to a pixel on screen
+	 return GetPen();
 }
 
-int getCharTouch(void) {
-	// poll Rx bit in 6850 status register. Wait for it to become '1'
-	// read received characer from 6850 RxData resgister.
-	int read_status_bit = 0;
-	while(read_status_bit == 0) {
-		read_status_bit = Touch_Status & 0b01;
-	}
-	int character = Touch_Rx;
-	return character;
-}
-
-int main (void){
+void TouchLoop(){
 	Init_Touch();
 	while(1){
 		GetPress();
-		GetRelease();
+	  	GetRelease();
 	}
-	return 0;
+}
 
+int main()
+{
+  printf("Hello from Nios II!\n");
+
+  TouchLoop();
+
+  return 0;
 }
