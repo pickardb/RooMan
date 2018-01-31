37f9bfb629e2d20f85b029ebd669a493ff1e49e7
Samuel Farinas <samuelfarinas@gmail.com>
2018-01-31 14:19:42 -0800

    Add working Exercise 1.5.

---
+++ exercise1_5/NIOS_II_System_391_For_Students/Sram_inst.vhd
@@ -0,0 +1,7 @@
+Sram_inst : Sram PORT MAP (
+		address	 => address_sig,
+		clock	 => clock_sig,
+		data	 => data_sig,
+		wren	 => wren_sig,
+		q	 => q_sig
+	);
---
+++ exercise1_5/NIOS_II_System_391_For_Students/software/exercise13/obj/default/hello_world.d
@@ -0,0 +1,6 @@
+obj/default/hello_world.o: hello_world.c ../exercise13_bsp//HAL/inc/io.h \
+ ../exercise13_bsp//HAL/inc/alt_types.h
+
+../exercise13_bsp//HAL/inc/io.h:
+
+../exercise13_bsp//HAL/inc/alt_types.h:
